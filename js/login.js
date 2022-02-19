8bc1f2725cf651a5a19839a3f384675aa8aec857
karineudahl <74554925+karineudahl@users.noreply.github.com>
2022-02-19 21:01:25 +0100

    up

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 import createMenu from "./ui/createMenu.js";
-import { baseUrl  } from "./settings/api.js";
+import { baseUrl } from "./settings/api.js";
 import { displayMessage } from "./ui/displayMessage.js";
-import { getFromStorage, saveToStorage } from "./utils/localStorage.js"
+import { saveToStorage } from "./utils/localStorage.js"
 import { tokenKey, userKey } from "./settings/variables.js";
 
 createMenu(); 
@@ -22,7 +22,7 @@ function tryTologIn(event) {
     const passwordValue = password.value.trim(); 
 
     if(usernameValue.length === 0 || passwordValue.length === 0) {
-        return displayMessage("error", "Username and/or password is wrong", ".message-container"); 
+        return displayMessage("error", "Username and/o r password is wrong", ".message-container"); 
     }
 
     loggedIn(usernameValue, passwordValue); 
@@ -45,14 +45,9 @@ async function loggedIn(username, password) {
         const response = await fetch(url, options); 
         const json = await response.json(); 
 
-        console.log(json); 
-
         if(json.user) {
-            // displayMessage ("success", "You are logged in", ".message-container");
-
             saveToStorage(tokenKey, json.jwt);
             saveToStorage(userKey, json.user);
-
             location.href = "/change-product.html"; 
         }
 
@@ -62,6 +57,54 @@ async function loggedIn(username, password) {
 
     }
     catch(error) {
-        console.log(error)
+        displayMessage ("error", "An error has occured", ".message-container");
     }
 }
+
+// function tryTologIn(event) {
+//     event.preventDefault(); 
+
+//     messageContainer.innerHTML = ""; 
+
+//     const usernameValue = username.value.trim();
+//     const passwordValue = password.value.trim(); 
+
+//     if(usernameValue.length === 0 || passwordValue.length === 0) {
+//         return displayMessage("error", "Username and/or password is wrong", ".message-container"); 
+//     }
+
+//     loggedIn(usernameValue, passwordValue); 
+// }
+
+// async function loggedIn(username, password) {
+//     const url = baseUrl + "auth/local"; 
+
+//     const data = JSON.stringify( { identifier: username, password: password} )
+
+//     const options = {
+//         method: "POST", 
+//         body: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     }; 
+
+//     try {
+//         const response = await fetch(url, options); 
+//         const json = await response.json(); 
+
+//         if(json.user) {
+//             saveToStorage(tokenKey, json.jwt);
+//             saveToStorage(userKey, json.user);
+//             location.href = "/change-product.html"; 
+//         }
+
+//         if(json.error) {
+//             displayMessage ("error", "Username and/or password is wrong", ".message-container"); 
+//         }
+
+//     }
+//     catch(error) {
+//         displayMessage ("error", "An error has occured", ".message-container");
+//     }
+// }
