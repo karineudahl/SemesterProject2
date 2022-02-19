dfd9d6fc029b7f80bcdb013d6f55b996c97b494e
karineudahl <74554925+karineudahl@users.noreply.github.com>
2022-02-19 21:36:23 +0100

    update

--- js/login.js
+++ js/login.js
@@ -3,34 +3,46 @@ import { baseUrl } from "./settings/api.js";
 import { displayMessage } from "./ui/displayMessage.js";
 import { saveToStorage } from "./utils/localStorage.js"
 import { tokenKey, userKey } from "./settings/variables.js";
+import { checkLength } from "./ui/checkLength.js";
 
 createMenu(); 
 
 const logInForm = document.querySelector(".login-form"); 
 const username = document.querySelector("#username"); 
+const usernameError = document.querySelector("#usernameError");
 const password = document.querySelector("#password");
+const passwordError = document.querySelector("#passwordError"); 
 const messageContainer = document.querySelector(".message-container");
 
 logInForm.addEventListener("submit", tryTologIn); 
 
 function tryTologIn(event) {
     event.preventDefault(); 
-
     messageContainer.innerHTML = ""; 
 
-    const usernameValue = username.value.trim();
-    const passwordValue = password.value.trim(); 
-
-    if(usernameValue.length === 0 || passwordValue.length === 0) {
-        return displayMessage("error", "Username and/o r password is wrong", ".message-container"); 
+    if(checkLength(username.value, 5)) {
+        usernameError.style.display = "none";
+    } else {
+        usernameError.style.display = "block";
+    }
+    
+    if(checkLength(password.value, 6)) {
+        passwordError.style.display = "none";
+    } else {
+        passwordError.style.display = "block";
     }
 
-    loggedIn(usernameValue, passwordValue); 
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();  
+
+    if(checkLength(username.value, 5) && checkLength(password.value, 6)) {
+        loggedIn(usernameValue, passwordValue);
+    }
 }
 
 async function loggedIn(username, password) {
-    const url = baseUrl + "auth/local"; 
 
+    const url = baseUrl + "auth/local"; 
     const data = JSON.stringify( { identifier: username, password: password} )
 
     const options = {
@@ -60,51 +72,3 @@ async function loggedIn(username, password) {
         displayMessage ("error", "An error has occured", ".message-container");
     }
 }
-
-// function tryTologIn(event) {
-//     event.preventDefault(); 
-
-//     messageContainer.innerHTML = ""; 
-
-//     const usernameValue = username.value.trim();
-//     const passwordValue = password.value.trim(); 
-
-//     if(usernameValue.length === 0 || passwordValue.length === 0) {
-//         return displayMessage("error", "Username and/or password is wrong", ".message-container"); 
-//     }
-
-//     loggedIn(usernameValue, passwordValue); 
-// }
-
-// async function loggedIn(username, password) {
-//     const url = baseUrl + "auth/local"; 
-
-//     const data = JSON.stringify( { identifier: username, password: password} )
-
-//     const options = {
-//         method: "POST", 
-//         body: data,
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     }; 
-
-//     try {
-//         const response = await fetch(url, options); 
-//         const json = await response.json(); 
-
-//         if(json.user) {
-//             saveToStorage(tokenKey, json.jwt);
-//             saveToStorage(userKey, json.user);
-//             location.href = "/change-product.html"; 
-//         }
-
-//         if(json.error) {
-//             displayMessage ("error", "Username and/or password is wrong", ".message-container"); 
-//         }
-
-//     }
-//     catch(error) {
-//         displayMessage ("error", "An error has occured", ".message-container");
-//     }
-// }
