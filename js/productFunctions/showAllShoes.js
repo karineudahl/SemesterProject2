9f01a91605e8df1742819870da25d4b7889bbd27
karineudahl <74554925+karineudahl@users.noreply.github.com>
2022-02-28 11:31:44 +0100

    update

--- js/productFunctions/showAllShoes.js
+++ js/productFunctions/showAllShoes.js
@@ -12,10 +12,10 @@ export function showAllShoes(products) {
 
         products.forEach(function(product) {
             shoesContainer.innerHTML += `<div class="product-content">
-                                            <a href="products-details.html?id=${product.id}"> 
+                                            <a href="products-details.html?id=${product.id}" aria-label="${product.title}"> 
                                                 <img src="${product.image.formats.large.url}" alt="${product.title}"></img>                                         
                                                 <h2>${product.title}<h2>
-                                                <p>kr. ${product.price}<p> 
+                                                <p>kr. ${product.price}<p>   
                                             </a>
                                         </div>` 
         }); 
@@ -26,7 +26,7 @@ export function showAllShoes(products) {
 
         products.forEach(function(product) {
             shoesContainer.innerHTML += `<div class="product-content">
-                                            <a href="edit-delete-product.html?id=${product.id}"> 
+                                            <a href="edit-delete-product.html?id=${product.id}" aria-label="${product.title}"> 
                                                 <img src="${product.image.formats.large.url}" alt="${product.title}"></img>                                         
                                                 <h2>${product.title}<h2>
                                                 <p>kr. ${product.price}<p> 
